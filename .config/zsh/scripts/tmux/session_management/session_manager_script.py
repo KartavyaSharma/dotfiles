47d27c789d82daccb728cc43ced89cc482a132ac
Kartavya Sharma <kartavya@berkeley.edu>
2023-04-24 18:20:16 -0700

    Refactored session manager script

--- .config/zsh/scripts/tmux/session_management/session_manager_script.py
+++ .config/zsh/scripts/tmux/session_management/session_manager_script.py
@@ -2,44 +2,41 @@ import fire
 import subprocess
 
 
+def create_subprocess(command):
+    """
+    Creates a subprocess for the string `command`
+    """
+    return subprocess.run(command, shell=True,
+                          text=True, check=True, stdout=subprocess.PIPE)
+
+
 def create():
     # Final session name store
     final_session_name = ""
     try:
         # Confirm if you want to create a new space or choose a scope
-        subprocess.run("gum confirm \"Choose scope?\" && echo \"True\"",
-                       shell=True, text=True,
-                       check=True, stdout=subprocess.PIPE
-                       )
+        create_subprocess("gum confirm \"Choose scope?\" && echo \"True\"")
         # Get all ongoing courses
-        ongoing = subprocess.run("echo \"Choose the scope of this new tmux space\""
-                                 "&& cat ongoing_courses.txt | gum filter",
-                                 shell=True, check=True,
-                                 text=True, stdout=subprocess.PIPE
-                                 )
+        ongoing = create_subprocess("echo \"Choose the scope of this new tmux space\""
+                                    "&& cat ongoing_courses.txt | gum filter")
         chosen = ongoing.stdout.strip().split("\n")[1]
         # Ask if you want to add any extensions to the name
-        extension = subprocess.run(
-            "gum input --placeholder \"Enter extension, if any.\"",
-            shell=True, check=True, text=True, stdout=subprocess.PIPE
-        )
+        extension = create_subprocess(
+            "gum input --placeholder \"Enter extension, if any.\"",)
         extension = "_" + extension.stdout.strip()
         extension = "" if extension == "_" else extension
         # Construct final session name
         final_session_name = chosen + extension
     except Exception:
         # Ask for custom session name
-        custom_name = subprocess.run(
-            "gum input --placeholder \"Enter custom tmux session name.\"",
-            shell=True, check=True, text=True, stdout=subprocess.PIPE
-        )
+        custom_name = create_subprocess(
+            "gum input --placeholder \"Enter custom tmux session name.\"",)
         final_session_name = custom_name.stdout.strip()
 
     # Create new tmux session with final name
-    subprocess.run(f"tmux new-session -d -s \"{final_session_name}\""
-                   f"&& tmux switch -t \"{final_session_name}\"",
-                   shell=True, check=True, text=True
-                   )
+    create_subprocess(f"tmux new-session -d -s \"{final_session_name}\""
+                      f"&& tmux switch -t \"{final_session_name}\""
+                      )
     print("Created new tmux session: " + final_session_name)
 
 
