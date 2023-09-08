5158106ae5e5d47ea1b086bbd825b908adeac605
Kartavya Sharma <kartavya@berkeley.edu>
2023-09-08 13:27:56 -0700

    Updated validate.py script with global paths

--- .config/zsh/scripts/utils/validate_pass/validate.py
+++ .config/zsh/scripts/utils/validate_pass/validate.py
@@ -1,15 +1,16 @@
 import hashlib
 import fire
+import os
 
 
 def validate(password=""):
     # Get salt file
     salt = ""
-    with open("salt", "r") as f:
+    with open(os.path.dirname(__file__) + "/salt", "r") as f:
         salt = [line for line in f.readlines()][0].strip()
     # Get compare hash
     hash = ""
-    with open("enc_sudo", "r") as f:
+    with open(os.path.dirname(__file__) + "/enc_sudo", "r") as f:
         hash = [line for line in f.readlines()][0].strip()
     # Create salted string
     salted = str(password) + salt
